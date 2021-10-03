fd06aada57ea750459615f2b6865b46bd2f7c9d6
Arsentii Tatarenko <tatarenkoarsen@gmail.com>
2021-10-03 12:12:50 +0300

    export many  objects to calc

--- assets/module2/index.js
+++ assets/module2/index.js
@@ -1,7 +1,7 @@
 "use strict";
 
-import { sum, sub, mult } from "../module1/index.js";
+import * as calc from "../module1/index.js";
 
-console.log(sum(2, 4));
-console.log(sub(2, 4));
-console.log(mult(2, 4));
+console.log(calc.sum(2, 5));
+console.log(calc.sub(2, 5));
+console.log(calc.mult(2, 5));
